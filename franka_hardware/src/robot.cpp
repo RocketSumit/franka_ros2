eb276c8ee89b6588f291e919f93f12cec95966bd
JafarAbdi <jafar.uruc@gmail.com>
2023-04-06 14:09:02 +0000

    Reduce the values to 50

--- franka_hardware/src/robot.cpp
+++ franka_hardware/src/robot.cpp
@@ -32,11 +32,21 @@ Robot::Robot(const std::string& robot_ip, const rclcpp::Logger& logger) {
         "You are not using a real-time kernel. Using a real-time kernel is strongly recommended!");
   }
   robot_ = std::make_unique<franka::Robot>(robot_ip, rt_config);
+  // Set the collision behavior.
+  std::array<double, 7> lower_torque_thresholds_acceleration{
+      {50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0}};
+  std::array<double, 7> upper_torque_thresholds_acceleration{
+      {50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0}};
+  std::array<double, 6> lower_force_thresholds_acceleration{{50.0, 50.0, 50.0, 50.0, 50.0, 50.0}};
+  std::array<double, 6> upper_force_thresholds_acceleration{{50.0, 50.0, 50.0, 50.0, 50.0, 50.0}};
+  robot_->setCollisionBehavior(
+      lower_torque_thresholds_acceleration, upper_torque_thresholds_acceleration,
+      lower_force_thresholds_acceleration, upper_force_thresholds_acceleration);
   // set collision behavior
-  robot_->setCollisionBehavior({{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
-                             {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
-                             {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
-                             {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}});
+  /* robot_->setCollisionBehavior({{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}}, */
+  /*                            {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}}, */
+  /*                            {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}}, */
+  /*                            {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}}); */
 }
 
 void Robot::write(const std::array<double, 7>& efforts) {
